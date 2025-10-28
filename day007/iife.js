361fa47378a01d2467baa289ba8dbf872bd783be
Ankit00011 <ankitsharmaji0981@gmail.com>
2025-10-28 11:01:31 +0530

    changeAll

---
+++ day007/iife.js
@@ -0,0 +1,4 @@
+// Immediately Invoked Function Expression
+(function greet(){
+    console.log("hello hii");
+})();
---
+++ day007/loops.js
@@ -0,0 +1,6 @@
+const n =[1,2,3,4,5,6,7,8,9];
+for(let i=0; i <n.length ;i++){
+   console.log(n[i]);
+}
+
+n.forEach((ele)=>console.log(ele));
