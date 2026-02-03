cc9748eb08c5c5c254307be5c30aef9addd32359
Ankit00011 <ankitsharmaji0981@gmail.com>
2026-02-03 09:28:56 +0530

    m

---
+++ day 09/promise.js
@@ -0,0 +1,12 @@
+const myPromise=new Promise((resolve,reject)=>{
+    let age =19;
+    if(age>18){
+        resolve("eligible for vote")
+    }
+    else{
+        reject("Not elegible")
+    }
+})
+//console.log(myPromise);
+
+myPromise.then((msg)=> console.log(msg))
